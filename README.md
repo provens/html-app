859614a5a86fd466d613ed1530c31c7eb8243932
Damian <sesepro@gmail.com>
2017-07-14 22:36:53 +0200

    Upload

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # html-app
 Simple HTML app for a project
+Used my different app and changed a few things around
---
+++ js/canvas.js
@@ -0,0 +1,4 @@
+var ctx = document.getElementById("MyCanvas").getContext("2d");
+ctx.beginPath();
+ctx.arc(170,170,160,0,Math.PI*2,true);
+ctx.stroke();
